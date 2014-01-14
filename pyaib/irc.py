d31c561d6828ee12db9d164b8004cbc222ed78c7
Jason Fried <me@jasonfried.info>
2014-01-13 18:21:11 -0800

    Pep8

--- pyaib/irc.py
+++ pyaib/irc.py
@@ -326,6 +326,7 @@ Message.add_parser('PRIVMSG', Message._directed_message)
 Message.add_parser('NOTICE', Message._directed_message)
 Message.add_parser('INVITE', Message._directed_message)
 
+
 class Sender(unicode):
     """all the logic one would need for understanding sender part of irc msg"""
     def __new__(cls, sender):
