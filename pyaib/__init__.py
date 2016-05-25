403cdf0c6289c7599f16240aa6c6c9215ffe4a7e
Jason Fried <fried@fb.com>
2016-05-25 12:55:42 -0700

    bumping version number

--- pyaib/__init__.py
+++ pyaib/__init__.py
@@ -24,7 +24,7 @@ __version_info__ = namedtuple(
     'version_info',
     'major minor micro releaselevel serial')(major=2,
                                              minor=0,
-                                             micro=0,
+                                             micro=1,
                                              releaselevel='final',
                                              serial=0)
 
